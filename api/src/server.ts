0e73184b40f102d84b220de1efcade126607b254
Aryan Singh <aryansingh920@outlook.com>
2026-03-29 23:07:48 +0530

    feat: add Prometheus metrics support; integrate prom-client and custom metrics for event acknowledgments

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,11 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import path from "path";
 import dotenv from "dotenv";
-import morgan from "morgan"; // 1. Import Morgan
+import morgan from "morgan";
 import myRouter from "./router/routes.v1";
 import { pool } from "./helper/db";
-
-
+import client from "prom-client"; // 1. Import prom-client
 
 const envPath = path.resolve(__dirname, ".env");
 dotenv.config({ path: envPath });
@@ -14,25 +13,47 @@ dotenv.config({ path: envPath });
 const app: Application = express();
 const PORT = process.env.API_PORT;
 
+// --- Prometheus Setup ---
+const register = new client.Registry();
+
+// Add default metrics (CPU, RAM, etc.)
+client.collectDefaultMetrics({ register });
+
+// 2. Custom Metric for Acknowledgments
+export const acknowledgmentsCounter = new client.Counter({
+  name: "total_events_acknowledged",
+  help: "Total number of events successfully acknowledged by moderators",
+  labelNames: ["region"],
+});
+register.registerMetric(acknowledgmentsCounter);
+
+// --- DB Connection ---
 async function testDBConnection() {
   try {
-    const client = await pool.connect();
+    const dbClient = await pool.connect();
     console.log("Successfully connected to the database!");
-
-    client.release(); // Critical: Always release the client back to the pool
+    dbClient.release();
   } catch (err: any) {
     console.error("Database connection error:", err.stack);
   }
 }
-
 testDBConnection();
 
 // --- Middleware ---
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms [:date[clf]]'));
-
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms [:date[clf]]",
+  ),
+);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// --- Metrics Endpoint ---
+app.get("/metrics", async (req: Request, res: Response) => {
+  res.setHeader("Content-Type", register.contentType);
+  res.send(await register.metrics());
+});
 
 // --- Routes ---
 app.use("/api", myRouter);
